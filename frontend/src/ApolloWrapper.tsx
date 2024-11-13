d0514db5aadc866f1af17f62122534d09e0387db
Mihails <m.murasovs@gmail.com>
2024-11-13 16:04:17 +0100

    FE: use prod on fe

--- frontend/src/ApolloWrapper.tsx
+++ frontend/src/ApolloWrapper.tsx
@@ -10,8 +10,8 @@ function makeClient() {
     return new ApolloClient({
         cache: new InMemoryCache(),
         link: new HttpLink({
-            // uri: "https://places-i-ate.onrender.com/graphql",
-            uri: "http://localhost:8080/graphql",
+            uri: "https://places-i-ate.onrender.com/graphql",
+            // uri: "http://localhost:8080/graphql",
         }),
     });
 }
