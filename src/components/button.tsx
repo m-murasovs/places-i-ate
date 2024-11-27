826d8753a507b845baa5474f6747e1553feebe22
Mihails <m.murasovs@gmail.com>
2024-11-27 17:10:19 +0100

    Improve Button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,18 +1,30 @@
 import React, { ReactElement } from 'react';
 
-export const PrimaryButton = ({
-    children,
-    type,
-    ...props
-}: {
-    type?: 'submit' | 'reset' | 'button' | undefined;
+interface Button {
     children: ReactElement | string;
-}) => {
+    type?: 'submit' | 'reset' | 'button' | undefined;
+    className?: string;
+}
+
+const BUTTON_DEFAULT_STYLE = 'font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline';
+
+const ButtonBase = ({
+    children,
+    className,
+    ...props
+}: Button) => {
     return <button
-        type={type}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        className={`${className} ${BUTTON_DEFAULT_STYLE}`}
         {...props}
     >
         {children}
     </button>;
+};
+
+export const PrimaryButton = (props: Button) => {
+    return <ButtonBase className={`${props.className} bg-blue-500 hover:bg-blue-700 text-white`} {...props} />;
+};
+
+export const SecondaryButton = (props: Button) => {
+    return <ButtonBase className={`${props.className} bg-gray-200 hover:bg-gray-300`} {...props} />;
 };
